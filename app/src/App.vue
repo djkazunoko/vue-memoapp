d5aa4d643428a2948ce5aa0c4b76e38da882ffb2
djkazunoko <djkazunoko@gmail.com>
2023-01-16 14:59:21 +0900

    メモのタイトルをクリックしてメモの編集フォームを表示

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,22 +4,35 @@ import { ref } from "vue";
 let id = 0;
 
 const memos = ref([]);
+const editingMemo = ref();
 
 function addMemo() {
   const memo = { id: id++, content: "新規メモ" };
   memos.value.push(memo);
+}
+
+function editMemo(memo) {
+  editingMemo.value = memo;
 }
 </script>
 
 <template>
   <ul>
     <li v-for="memo in memos" :key="memo.id">
-      {{ memo.content }}
+      <a @click.prevent="editMemo(memo)" href="#">{{ memo.content }}</a>
     </li>
   </ul>
   <form @submit.prevent="addMemo">
     <button>新規作成</button>
   </form>
+  <div v-if="editingMemo">
+    <div>
+      <textarea v-model="editingMemo.content"></textarea>
+    </div>
+    <div>
+      <button>編集</button>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
