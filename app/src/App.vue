2640603ef101d8edc4d576deb54d8f7a809ffb3d
djkazunoko <djkazunoko@gmail.com>
2023-01-16 14:36:15 +0900

    メモの追加と一覧表示

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,25 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+let id = 0;
+
+const memos = ref([]);
+
+function addMemo() {
+  const memo = { id: id++, content: "新規メモ" };
+  memos.value.push(memo);
+}
+</script>
 
 <template>
-  <h1>Hello World!</h1>
+  <ul>
+    <li v-for="memo in memos" :key="memo.id">
+      {{ memo.content }}
+    </li>
+  </ul>
+  <form @submit.prevent="addMemo">
+    <button>新規作成</button>
+  </form>
 </template>
 
 <style scoped></style>
